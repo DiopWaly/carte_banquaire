67a6af5312b7766a2a755b4d50821367536cd4a8
Papa Waly Diop <waly0092@gmail.com>
2019-03-24 16:59:38 +0000

    Signed-off-by: Papa Waly Diop <waly0092@gmail.com>

--- src/MA/Verificateur.java
+++ src/MA/Verificateur.java
@@ -4,27 +4,38 @@ public abstract class Verificateur {
 	protected String num;
 	public static int[] validite(String num){
 		int i=0;
-		int p=0;
 		int s = 0;
 		int[] numero = new int[num.length()]; 
 		char[] ch = num.toCharArray();
 		for(int j = 0; j< numero.length; j++ ){
 			numero[j] = Integer.parseInt(String.valueOf(ch[j]));
+			System.out.print("["+numero[j]+"]");
 		}
-		for (int j = 0; j < (numero.length)/2; j++) {
-			i += (2*numero[2*j])%9;
+		for (int j = 0; j < (numero.length); j++) {
+			if(j%2 != 0)
+			   s += numero[j];
+			else{
+				 i = 2*numero[j];
+				 if(2*numero[j]>9){
+					 s += (i/10)+(i%10);
+			     }
+				 else
+					 s += 2*numero[j];
+			}		
 		}
-		for (int j = 0; j < (numero.length-1)/2; j++) {
-			
-			p += numero[2*j + 1];
-		}
-		s = i+p+numero[numero.length-1];
-		if(s%10 == 0)
+		if(s%10 == 0){
+			System.out.println("Carte valide ");
 		   return numero;
-		System.out.println("Numero "+num+" n'est pas valide !!!");
+		}
+		  System.out.println("Numero "+num+" n'est pas valide !!!");
 		return null;
 	}
 
 	public abstract void Verificator(String num);
+	public static void main(String[] args) {
+		System.out.println(validite("5326351191771263"));
+		System.out.println(validite("4978210033328381"));
+		
+	}
 	
 }
